08897e9359dca5ef8d01b81e551a0b1df649e8ac
Abhishek Chaudhary <abhishekchaudhary0220@gmail.com>
2020-09-24 19:30:04 +0530

    a star, best first

--- Assignment_1/distance.py
+++ Assignment_1/distance.py
@@ -5,6 +5,14 @@ sys.stdin = open("input.txt", 'r')
 
 from graph import Graph
 
+class Node:
+    def __init__(self, name:str, parent:str):
+        self.name = name
+        self.parent = parent
+        self.g = 0 # Distance to start node
+        self.h = 0 # Distance to goal node
+        self.f = 0 # Total cost
+
 class Min_distance:
     def __init__(self, graph : Graph, start, end):
         self.graph = graph
@@ -53,10 +61,6 @@ class Min_distance:
 
     def bfs(self):
         self.reset()
-        distace_from_start = {}
-        for i in self.graph.all_nodes():
-            distace_from_start[i] = math.inf
-
         path = deque()
         path.append((self.start,0))
         queue = deque()
@@ -77,8 +81,48 @@ class Min_distance:
                     new_path.append((b, dist_till+dist))
                     queue.append(new_path)
         print(self.path_taken)
-        
 
+    def dist_huristics(self, heuristics, g, h):
+        self.reset()
+        open = []
+        closed = []
+        start_node = Node(self.start, None)
+        goal_node = Node(self.end, None)
+        open.append(start_node)
+        while len(open) > 0:
+            open.sort(key= lambda z: z.g*g + z.h*h)
+            current_node = open.pop(0)
+            closed.append(current_node)
+            if current_node.name == goal_node.name:
+                path = []
+                while current_node.name != start_node.name:
+                    path.append((current_node.name,current_node.g))
+                    current_node = current_node.parent
+                path.append((start_node.name, start_node.g))
+                return path[::-1]
+            neighbors = self.graph.get(current_node.name)
+            for key, value in neighbors.items():
+                neighbor = Node(key, current_node)
+                if(neighbor in closed):
+                    continue
+                neighbor.g = current_node.g + graph.get(current_node.name, neighbor.name)
+                neighbor.h = heuristics.get(neighbor.name)
+                neighbor.f = neighbor.h
+                if(self.add_to_open(open, neighbor, g, h) == True):
+                    open.append(neighbor)
+
+    def add_to_open(self, open, neighbor, g = 1, h = 1):
+        for node in open:
+            if (neighbor.name == node.name and ((neighbor.g*g + neighbor.h*h) >= (node.g*g + node.h*h))):
+                return False
+        return True
+    
+    def a_star(self, heuristics):
+        print(self.dist_huristics(heuristics, 1, 1))
+    
+    def best_first_search(self, heuristics):
+        print(self.dist_huristics(heuristics, 0, 1))
+    
 
 graph = Graph()
 graph.make_undirected()
@@ -86,12 +130,25 @@ for i in range(int(input().rstrip())):
     a,b,dist = input().rstrip().split()
     graph.connect(a, b, int(dist))
 
+heuristics = {}
+for i in range(int(input().rstrip())):
+    a,b =  input().rstrip().split()
+    heuristics[a] = int(b)
+
 distance = Min_distance(graph, 'Frankfurt', 'Ulm')
+
+
 time_start = time.time()
 distance.dfs()
 print("time taken by DFS", time.time() - time_start)
 time_start = time.time()
 distance.bfs()
 print("time taken by BFS", time.time() - time_start)
+time_start = time.time()
+distance.a_star(heuristics)
+print("time taken by a_star", time.time() - time_start)
+time_start = time.time()
+distance.best_first_search(heuristics)
+print("time taken by best_first_search", time.time() - time_start)
 
 
